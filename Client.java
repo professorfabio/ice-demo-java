de99ffd88638098ce8ae2b1e8541cc7a392dcd32
Fábio M. Costa <fmc@ufg.br>
2026-04-15 18:21:20 -0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,7 +7,7 @@ public class Client {
 
             // 2. Create a proxy to the remote 'Printer' object
             // Replace '10.0.0.5' with the actual IP of your ICE server
-            ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -h 98.90.53.6 -p 11000");
+            ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -h 98.90.53.6 -p 5678");
 
             // 3. Downcast the proxy to the Printer interface
             Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
@@ -17,7 +17,7 @@ public class Client {
             }
 
             // 4. Call the remote method
-            String response = printer.printString("Hello from Amazon Linux 2023!");
+            String response = printer.printString("Hello from Goiania!");
             System.out.println("Server responded: " + response);
 
         } catch (LocalException e) {
